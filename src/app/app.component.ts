419a89dc42b7ce51ff71037da3ca2aa70aa2543c
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2023-10-24 18:07:24 +0200

    feat: init Angular as standalone

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {}
