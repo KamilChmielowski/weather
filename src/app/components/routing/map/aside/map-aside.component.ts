23618496569bd75e01b6852080a826d1d777f7d7
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2023-11-25 10:18:58 +0100

    feat: map aside layout

--- src/app/components/routing/map/aside/map-aside.component.ts
+++ src/app/components/routing/map/aside/map-aside.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SvgIconComponent } from 'angular-svg-icon';
+import { CityItemComponent } from '../../cities/main/city-item/city-item.component';
 
 @Component({
   selector: 'app-map-aside',
@@ -9,9 +10,10 @@ import { SvgIconComponent } from 'angular-svg-icon';
   templateUrl: './map-aside.component.html',
   styleUrls: ['./map-aside.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    CommonModule,
-    SvgIconComponent,
-  ],
+    imports: [
+        CommonModule,
+        SvgIconComponent,
+        CityItemComponent,
+    ],
 })
 export class MapAsideComponent {}
